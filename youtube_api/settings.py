5319d03bbdee09d7189a50e38cae7e94ae02a57c
Anartz Mugika <amugika@codesyntax.com>
2015-08-07 09:51:41 +0200

    Return to get data from SQLite db

--- youtube_api/settings.py
+++ youtube_api/settings.py
@@ -75,15 +75,15 @@ WSGI_APPLICATION = 'youtube_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'djangogirls-anartz',                      # Or path to database file if using sqlite3.
@@ -92,7 +92,7 @@ DATABASES = {
         'HOST': 'mysql.cs',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
-}
+}"""
 
 
 # Internationalization
