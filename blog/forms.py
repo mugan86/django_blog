78c915c1a2d09beb8df6b46eabe9de5132d49289
Anartz Mugika <amugika@codesyntax.com>
2015-08-07 10:28:53 +0200

    Extends app with show post details in new template

--- blog/forms.py
+++ blog/forms.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'text','published_date')
+        fields = ('title', 'text')
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,3 +7,7 @@ def post_list(request):
     # import pdb
     # pdb.set_trace()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
