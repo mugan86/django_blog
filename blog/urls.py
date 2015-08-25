e851ccef372878c73ef6ebc23fa403bbd0e78ab6
Anartz Mugika <amugika@codesyntax.com>
2015-08-25 08:32:53 +0200

    Update 'contact url reference

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^about', views.about),
-    url(r'^contact', views.contact),
+    url(r'^contact/', views.contact_new, name='contact_new'),
 ]
