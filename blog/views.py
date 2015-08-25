f5ab7b905c3f5249a6dc025d965c6bc5656e0781
Anartz Mugika <amugika@codesyntax.com>
2015-08-25 08:29:03 +0200

    Add contact_new function and delete 'contact' function to use in contact form

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from .forms import ContactForm
 
 def post_list(request):
     # Add "-" inside 'order by' element to change publish order
@@ -42,8 +43,25 @@ def post_edit(request, pk):
             form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form, 'title': title})
 
+def contact_new(request):
+
+    title = "Add new post"
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+            # import pdb
+            # pdb.set_trace()
+            return render(request, 'blog/post_list.html', {'posts': posts})
+    else:
+        form = ContactForm()
+
+    return render(request, 'blog/post_edit.html', {'form': form, 'title': title})
+
 def about(request):
     return render(request, 'blog/about.html')
 
-def contact(request):
-    return render(request, 'blog/contact.html')
+"""def contact(request):
+    return render(request, 'blog/contact.html')"""
