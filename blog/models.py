335449a25f732304e4109da50a1a203ed7ad51e7
Anartz Mugika <amugika@codesyntax.com>
2015-08-25 08:31:00 +0200

    Update Post model with new field called 'source'

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     text = models.TextField()
+    source = models.CharField(max_length=250)
     created_date = models.DateTimeField(
                 default=timezone.now)
     published_date = models.DateTimeField(
