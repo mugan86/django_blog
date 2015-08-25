2997a290e91bed4c07395ed4592bd8d23a6f8ccd
Anartz Mugika <amugika@codesyntax.com>
2015-08-25 07:46:28 +0200

    Add ContactAdmin list_display fields in Admin Panel

--- blog/admin.py
+++ blog/admin.py
@@ -7,7 +7,10 @@ from .models import Contact
 class PostAdmin(admin.ModelAdmin):
     list_display = ('author', 'title', 'description', 'created_date','published_date', )
 
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email','received_date', )
+
 admin.site.register(Post, PostAdmin)
 #Add create model in admin site to show
 admin.site.register(Category)
-admin.site.register(Contact)
+admin.site.register(Contact, ContactAdmin)
