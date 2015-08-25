89700a76c9a72db8b0539d99584f6098b39072fe
Anartz Mugika <amugika@codesyntax.com>
2015-08-25 07:10:44 +0200

    Show Contact model data in Admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import Post
 from .models import Category
+from .models import Contact
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('author', 'title', 'description', 'created_date','published_date', )
-    
+
 admin.site.register(Post, PostAdmin)
 #Add create model in admin site to show
 admin.site.register(Category)
+admin.site.register(Contact)
