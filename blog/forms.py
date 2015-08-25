619d6d0e1aaa25576e82a023ae311d6ba093b7d8
Anartz Mugika <amugika@codesyntax.com>
2015-08-25 07:45:41 +0200

    Add Contact form fields in forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Post
+from .models import Contact
 
 class PostForm(forms.ModelForm):
 
@@ -7,3 +8,10 @@ class PostForm(forms.ModelForm):
         model = Post
         # Fields to show in form
         fields = ('title', 'description', 'text')
+
+class ContactForm(forms.ModelForm):
+
+    class Meta:
+        model = Contact
+        # Fields to show in form
+        fields = ('name', 'email', 'message', 'received_date')
