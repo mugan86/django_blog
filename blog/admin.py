78adba369d658ba9a8a24e91e9b8b5459facaaff
Anartz Mugika <amugika@codesyntax.com>
2015-08-26 09:31:13 +0200

    Add Projects object in Admin Panel

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Post
 from .models import Category
 from .models import Contact
+from .models import Project
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -10,8 +11,12 @@ class PostAdmin(admin.ModelAdmin):
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('name', 'email','received_date', )
 
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'category', 'published_date')
+
 admin.site.register(Post, PostAdmin)
 
 #Add create model in admin site to show
 admin.site.register(Category)
 admin.site.register(Contact, ContactAdmin)
+admin.site.register(Project, ProjectAdmin)
