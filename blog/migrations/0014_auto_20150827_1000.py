dd0ca4280c65e98a57033a24e7ce4b3f21fbb092
Anartz Mugika <amugika@codesyntax.com>
2015-08-27 10:11:37 +0200

    Migration file add

---
+++ blog/migrations/0014_auto_20150827_1000.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import ckeditor.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0013_project_active'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='text',
+            field=ckeditor.fields.RichTextField(),
+        ),
+    ]
