ec3e02b9e59e16e22bfbd4879c44afa7a9960102
amugika <mugan86@gmail.com>
2015-08-31 21:49:57 +0200

    Add new url to filt post list by category

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
 	url(r'^$', views.principal),
     url(r'^blog', views.post_list),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^post/category/(?P<category>[a-zA-Z]+)/$', views.post_list_by_category), ##To test...
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^about', views.about),
