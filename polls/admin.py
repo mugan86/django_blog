145a00364f7d41440706391105aaf39f4b1b0a07
Anartz Mugika <mugan86@gmail.com>
2015-09-03 15:34:50 +0200

    Add new implementations in poll app admin

--- polls/admin.py
+++ polls/admin.py
@@ -5,9 +5,21 @@ from .models import Choice
 
 
 # Register your models here.
+#Items to Choice model
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'pub_date')
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_filter = ['pub_date']
+
 
 class ChoiceAdmin(admin.ModelAdmin):
     list_display = ('question', 'choice_text', 'votes')
