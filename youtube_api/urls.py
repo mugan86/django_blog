78378254c9ff676cc79655f53417705177a50d5e
amugika <mugan86@gmail.com>
2015-09-05 22:07:08 +0200

    Update 'polls main url with namespace'

--- youtube_api/urls.py
+++ youtube_api/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
     url(r'^ckeditor/', include('ckeditor.urls')),
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
 ]
