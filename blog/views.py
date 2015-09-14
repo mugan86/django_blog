724f33f3eb78466f06cc60465a4ba90b6a768c9a
Anartz Mugika <mugan86@gmail.com>
2015-09-14 11:42:12 +0200

    Generate PDF action add in view

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,26 @@ from .models import Post, Event, Friend
 from .forms import PostForm, EventForm, ContactForm
 from collections import Counter
 from django.db.models import Q
+#Generate PDF files
+from reportlab.pdfgen import canvas
+from django.http import HttpResponse
+
+def create_pdf(request):
+    # Create the HttpResponse object with the appropriate PDF headers.
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+
+    # Create the PDF object, using the response object as its "file."
+    p = canvas.Canvas(response)
+
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    p.drawString(100, 100, "Anartz Mugika!!")
+
+    # Close the PDF object cleanly, and we're done.
+    p.showPage()
+    p.save()
+    return response
 
 def principal(request):
 
