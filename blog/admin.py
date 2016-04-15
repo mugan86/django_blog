2f3fe5716b62b34cfea28e6e6fd14224c7471fab
Anartz Mugika <mugan86@gmail.com>
2016-04-15 11:05:18 +0200

    Add admin model admins different actions labels add with constant strings

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,13 @@
 from django.contrib import admin
 from .models import Post, PostType, Category, Contact, Project, Friend, FriendType, Event, EventType, Location
 
+#Add labels strings constant values
+mark_to_finish = u'Finish selected projects'
+mark_to_unfinish = u'No finish selected projects'
+
+publish_selected_projects = u'Publish selected projects'
+unpublish_selected_projects = u'Unpublish selected projects'
+
 #Task custom definitions
 def finish_project(self, request, queryset):
     queryset.update(finish = True)
@@ -21,8 +28,8 @@ class PostAdmin(admin.ModelAdmin):
     ordering = ('-published_date',)
     search_fields = ('title',)
     actions = [publish_action, unpublish_action]
-    publish_action.short_description = u'Publicar seleccionado'
-    unpublish_action.short_description = u'Despublicar seleccionado'
+    publish_action.short_description = publish_selected_projects
+    unpublish_action.short_description = unpublish_selected_projects
 
 class PostTypeAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'active', 'created_date','published_date', )
@@ -30,8 +37,8 @@ class PostTypeAdmin(admin.ModelAdmin):
     ordering = ('-published_date',)
     search_fields = ('description',)
     actions = [publish_action, unpublish_action]
-    publish_action.short_description = u'Publicar seleccionado'
-    unpublish_action.short_description = u'Despublicar seleccionado'
+    publish_action.short_description = publish_selected_projects
+    unpublish_action.short_description = unpublish_selected_projects
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'created_date','published_date',)
@@ -42,8 +49,8 @@ class ContactAdmin(admin.ModelAdmin):
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'category', 'start_project_date', 'active', 'finish', 'published_date')
     actions = [finish_project, unfinished_tasks]
-    finish_project.short_description = u'Marcar como finalizado'
-    unfinished_tasks.short_description = u'Marcar como no finalizado'
+    finish_project.short_description = mark_to_finish
+    unfinished_tasks.short_description = mark_to_unfinish
 
 class FriendAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'category', 'img', 'url', 'active', 'type')
@@ -51,14 +58,14 @@ class FriendAdmin(admin.ModelAdmin):
     ordering = ('-id',)
     search_fields = ('name',)
     actions = [publish_action, unpublish_action]
-    publish_action.short_description = u'Publicar seleccionado'
-    unpublish_action.short_description = u'Despublicar seleccionado'
+    publish_action.short_description = publish_selected_projects
+    unpublish_action.short_description = unpublish_selected_projects
 
 class FriendTypeAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'active', 'add_data')
     actions = [publish_action, unpublish_action]
-    publish_action.short_description = u'Publicar seleccionado'
-    unpublish_action.short_description = u'Despublicar seleccionado'
+    publish_action.short_description = publish_selected_projects
+    unpublish_action.short_description = unpublish_selected_projects
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'type', 'url', 'logotype' ,'celebrate_data')
