db6de9c288e374ab65abe94c88e5b6eba0a71123
Anartz Mugika <mugan86@gmail.com>
2016-07-05 10:37:26 +0200

    Register new url

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
 	url(r'^$', views.principal),
     url(r'^blog$', views.post_list),
     url(r'^report$', views.create_pdf),
+    url(r'^createpdf$', views.create_pdf_with_pisa),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
     url(r'^post/category/(?P<category>[a-zA-Z0-9]+)/$', views.post_list_by_category), ##To test...
     url(r'^post/new/$', views.post_new, name='post_new'),
